cf31b5c682fe9643e8dd9b1aa86bc63495557d02
Kyriakos Mandalas <kmandalas@cognity.gr>
2022-11-15 12:36:13 +0200

    updates properties

--- src/main/java/dev/kmandalas/urlshortener/ApplicationConfig.java
+++ src/main/java/dev/kmandalas/urlshortener/ApplicationConfig.java
@@ -13,10 +13,10 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 @EnableRedisRepositories
 public class ApplicationConfig {
 
-    @Value("${spring.redis.host}")
+    @Value("${spring.data.redis.host}")
     private String redisHost;
 
-    @Value("${spring.redis.port}")
+    @Value("${spring.data.redis.port}")
     private int redisPort;
 
     @Bean
